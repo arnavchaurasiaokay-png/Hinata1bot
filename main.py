edbd1a5ac07f5ce8081b42fc4d3ec046d56a42ae
arnavchaurasiaokay-png <arnavchaurasiaokay@gmail.com>
2026-03-31 14:18:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import asyncio
 from bot import Bot
 
-Bot().run()
+bot = Bot()
+
+async def main():
+    await bot.start()
+    print("Bot Running...")
+
+    # 🔥 Keep bot alive forever (IMPORTANT)
+    while True:
+        await asyncio.sleep(3600)
+
+if __name__ == "__main__":
+    asyncio.run(main())
